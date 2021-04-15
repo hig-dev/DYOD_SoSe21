6cf98ab26d1e5267d53cb4e4f81bc2a8caae3680
Halil Göcer <dev@hig.dev>
2021-04-15 03:43:02 +0200

    Implement value_segment

--- src/lib/storage/value_segment.cpp
+++ src/lib/storage/value_segment.cpp
@@ -14,18 +14,19 @@ namespace opossum {
 
 template <typename T>
 AllTypeVariant ValueSegment<T>::operator[](const ChunkOffset chunk_offset) const {
-  throw std::runtime_error("Implement ValueSegment::operator[]");
+  Assert(chunk_offset >= 0 && chunk_offset < size(), "The parameter \"chunk_offset\" is out of range.");
+  return _values[chunk_offset];
 }
 
 template <typename T>
 void ValueSegment<T>::append(const AllTypeVariant& val) {
-  // Implementation goes here
+  auto typed_val = type_cast<T>(val);
+  _values.emplace_back(typed_val);
 }
 
 template <typename T>
 ChunkOffset ValueSegment<T>::size() const {
-  // Implementation goes here
-  return 0;
+  return _values.size();
 }
 
 EXPLICITLY_INSTANTIATE_DATA_TYPES(ValueSegment);
